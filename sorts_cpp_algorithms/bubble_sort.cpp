04a43cb7fe20e2de5ad92c459077f6af77e92779
gunslinger527 <hs09949@st.habib.edu.pk>
2026-01-18 14:12:27 +0500

    Add files via upload

---
+++ sorts_cpp_algorithms/bubble_sort.cpp
@@ -0,0 +1,13 @@
+#include "sorts.h"
+#include <algorithm>
+
+void bubble_sort(std::vector<int>& v) {
+    int n = v.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (v[j] > v[j + 1]) {
+                std::swap(v[j], v[j + 1]);
+            }
+        }
+    }
+}
